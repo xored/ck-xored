be2bac917863cf091f486d92fe8386c19b0c9570
Dmitry Savenko <dsavenko@xored.com>
2017-05-29 12:30:00 +0700

    Minor

--- program/logouter/logouter.sh
+++ program/logouter/logouter.sh
@@ -4,6 +4,6 @@ cp "$CK_ENV_MODEL_CAFFE_LABELMAP" "labelmap_voc.prototxt"
 
 "./logouter" test --model=../test.prototxt --weights="$CK_ENV_MODEL_CAFFE_WEIGHTS" --gpu 0 | while read line; do
     if [ "$line" -eq "0" ]; then
-        gnome-screensaver-command -l
+        ${NO_PERSON_ACTION:-gnome-screensaver-command -l}
     fi
 done;
